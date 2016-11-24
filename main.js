04987515320914d5d370fd34ad4792a393882c76
Mark Hein <mark@heincountry.com>
2016-11-23 22:21:04 -0600

    test commit

---
+++ main.js
@@ -0,0 +1,43 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+
+ console.log('we live');
+
+  for(var name in Memory.creeps) {
+      if(!Game.creeps[name]) {
+          delete Memory.creeps[name];
+          console.log('Clearing non-existing creep memory:', name);
+      }
+  }
+
+    var tower = Game.getObjectById('7fcf3e0e09dea2771a81917f');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
