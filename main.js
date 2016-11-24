633a1e57ec575d742707fe5e5641da85948b856e
Mark Hein <mark@heincountry.com>
2016-11-23 22:35:21 -0600

    harvestors

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ module.exports.loop = function () {
 
  console.log('we live');
 
-  for(var name in Memory.creeps) {
-      if(!Game.creeps[name]) {
-          delete Memory.creeps[name];
-          console.log('Clearing non-existing creep memory:', name);
-      }
-  }
+ for(var name in Memory.creeps) {
+     if(!Game.creeps[name]) {
+         delete Memory.creeps[name];
+         console.log('Clearing non-existing creep memory:', name);
+     }
+ }
 
+ var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+ console.log('Harvesters: ' + harvesters.length);
+
+ if(harvesters.length < 2) {
+     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+     console.log('Spawning new harvester: ' + newName);
+ }
 
     var tower = Game.getObjectById('7fcf3e0e09dea2771a81917f');
     if(tower) {
